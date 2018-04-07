44c47da9f3a58c73c7bb7ec84441a48085f88374
Larspiproj <lars.gustavsson@live.com>
2018-04-07 17:57:22 +0000

    Merged dev1

--- solar.py
+++ solar.py
@@ -1,4 +1,4 @@
-#!/home/user/tracer_sql/venv/bin/python3
+#!/home/pi/tracer_sql/venv/bin/python3
 
 import serial
 from sqlalchemy import Column, DateTime, Integer, Float, Boolean, func
@@ -7,11 +7,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from tracer import Tracer, TracerSerial
 
-#fake = None
+fake = None
 # A sample response, to show what this demo does. Uncomment to use.
-fake = bytearray(b'\xEB\x90\xEB\x90\xEB\x90\x16\xA0\x18\xD2\x04\xD3\x04\x00\
-\x00\x0E\x00\x53\x04\xA5\x05\x01\x00\x00\x1F\x00\x00\x00\x01\x33\x0A\x0A\x00\
-\x9A\x38\x7F')
+#fake = bytearray(b'\xEB\x90\xEB\x90\xEB\x90\x16\xA0\x18\xD2\x04\xD3\x04\x00\
+#\x00\x0E\x00\x53\x04\xA5\x05\x01\x00\x00\x1F\x00\x00\x00\x01\x33\x0A\x0A\x00\
+#\x9A\x38\x7F')
 
 Base = declarative_base()
 
@@ -34,7 +34,20 @@ class Parameters(Base):
     batt_overload = Column(Boolean)
     batt_charging = Column(Boolean)
 
-engine = create_engine('sqlite:////home/user/tracer_sql/solar.sqlite')
+    def __repr__(self): 
+        return '{0} {1}\n    Battery Voltage={2} Battery Full Voltage={3} \
+Battery Overdischarge Voltage={4} Battery Temp={5} Solar Panel(PV) Voltage={6} \
+Charge Current={7}\n    Load On={8} Load Current={9} Load Overloaded={10} \
+Load Short={11} Battery Overloaded={12} Battery Overdischarged={13} \
+Battery Full={14}\n    Battery Charging={15}\n'.format(self.id, self.timestamp,
+                    self.batt_voltage, self.batt_full_voltage,
+                    self.batt_overdischarge_voltage, self.batt_temp,
+                    self.pv_voltage, self.charge_current, self.load_on,
+                    self.load_amps, self.load_overload, self.load_short,
+                    self.batt_overdischarge, self.batt_full,
+                    self.batt_overload, self.batt_charging)
+
+engine = create_engine('sqlite:////home/pi/tracer_sql/solar.sqlite')
 Session = sessionmaker()
 Session.configure(bind=engine)
 Base.metadata.create_all(engine)
@@ -68,6 +81,7 @@ def get_data():
     t_ser = TracerSerial(tracer, port)
     t_ser.send_command(0xA0)
     data = t_ser.receive_result()
+    port.close()
 
         # operating parameters
     p = Parameters(
@@ -93,19 +107,25 @@ def get_data():
 
 if (__name__) == ('__main__'):
     get_data()
-
-session.close()
-p = session.query(Parameters).all()
-parameters = open("/home/user/tracer_sql/parameters.txt", "w")
-for p in p:
-    parameters.write('{0} {1}\n    Battery Voltage:{2} Battery Full Voltage:{3} \
-Battery Overdischarge Voltage:{4} Battery Temp:{5} Solar Panel(PV) Voltage:{6} \
-Charge Current:{7}\n    Load On:{8} Load Current:{9} Load Overloaded:{10} \
-Load Short:{11} Battery Overloaded:{12} Battery Overdischarged:{13} \
-Battery Full:{14}\n    Battery Charging:{15}\n'.format(p.id, p.timestamp,
+    session.close()
+    p = session.query(Parameters).all()
+    print("Latest Reading:\n{}".format(p[-1]), end="")
+    #parameters = open("/home/pi/tracer_sql/parameters.log", "w")
+    with open("/home/pi/tracer_sql/parameters.log", "w") as f:
+        for p in p:
+            #parameters.write(str(p))
+            f.write(str(p))
+    """
+    for p in p:
+        parameters.write('{0} {1}\n    Battery Voltage={2} Battery Full Voltage={3} \
+Battery Overdischarge Voltage={4} Battery Temp={5} Solar Panel(PV) Voltage={6} \
+Charge Current={7}\n    Load On={8} Load Current={9} Load Overloaded={10} \
+Load Short={11} Battery Overloaded={12} Battery Overdischarged={13} \
+Battery Full={14}\n    Battery Charging={15}\n'.format(p.id, p.timestamp,
                     p.batt_voltage, p.batt_full_voltage,
                     p.batt_overdischarge_voltage, p.batt_temp, p.pv_voltage,
                     p.charge_current, p.load_on, p.load_amps, p.load_overload,
                     p.load_short, p.batt_overdischarge, p.batt_full,
                     p.batt_overload, p.batt_charging))
-parameters.close()
+    """
+    #parameters.close()
